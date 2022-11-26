9fae8ddf5611e53378ae2205d1201d5eafc446a5
alvaroLZ96 <alvarost54@gmail.com>
2022-11-26 13:38:24 +0100

    feat:nuevos tests en paintings

--- server/src/api/paintings/test/painting.test.js
+++ server/src/api/paintings/test/painting.test.js
@@ -1,48 +1,60 @@
 /* const supertest = require('supertest'); */
 const request = require('supertest');
 const { app } = require('../../../index');
-/* const PaintingRoutes = require('../painting.routes'); */
+/* const assert = require('assert');
+const mongoose = require('mongoose'); */
 
-/* const express = require('express');
-const app = new express();
-app.use('/', PaintingRoutes);
-
-const api = supertest(PaintingRoutes); */
 const newPainting = {
   name: `name_${new Date().getTime()}`,
   date: '2000',
   authorId: '637f50da8b678b516468737d',
 };
 
-test('GET paintings ', async () => {
-  const res = await request(app).get('/api/paintings');
-  expect(res.statusCode).toEqual(200);
-  expect(res.body.message).toEqual('Recovered all paintings');
-}, 10000);
+describe('GET paintings', () => {
+  test('all paintings returned', async () => {
+    const res = await request(app).get('/api/paintings');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toEqual('Recovered all paintings');
+  }, 10000);
 
-test('GET in Route Not Found', async () => {
-  const res = await request(app).get('/api/painting/hola');
-  expect(res.statusCode).toEqual(404);
-  expect(/Route Not Found/); //aquí falla porque añadimos un params inexistente
-}, 10000);
+  test('fail in getting the paintings', async () => {
+    const res = await request(app).get('/api/painting/hola');
+    expect(res.statusCode).toEqual(404);
+    expect(/Route Not Found/); //aquí falla porque añadimos un params inexistente
+  }, 10000);
+});
 
-test('Post a new painting', async () => {
-  await request(app)
-    .post('/api/paintings')
-    .send(newPainting)
-    .expect(201)
-    .expect((res) => {
-      expect(res.body.message).toEqual('Created Painting');
-    })
-    .then((response) => {
-      const { res } = response;
-      const jsonText = JSON.parse(res.text);
-      const { data } = jsonText;
-      newPainting._id = data.newPainting._id;
-    });
-}, 10000);
+describe('POST paintings', () => {
+  test('Post a new painting', async () => {
+    await request(app)
+      .post('/api/paintings')
+      .send(newPainting)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.message).toEqual('Created Painting');
+      })
+      .then((response) => {
+        const { res } = response;
+        const jsonText = JSON.parse(res.text);
+        const { data } = jsonText;
+        newPainting._id = data.newPainting._id;
+      });
+  }, 10000);
+  test.only('Post a new painting FAIL', async () => {
+    await request(app)
+      .post('/api/paintings')
+      .send({
+        nombre: `name_${new Date().getTime()}`,
+      })
+      .expect(500);
+    /*  .expect(res.body.message)
+    .toBe('Failed in painting post'); */
+    /* assert.equal(res.body.message, 'Resource Not Found'); */
+  }, 10000);
+});
 
-test('Delete a painting', async () => {
+describe('DELETE a painting');
+test('succeeds taking the ID', async () => {
   await request(app)
     .delete(`/api/paintings/${newPainting._id}`)
     .expect(200)
@@ -50,13 +62,13 @@ test('Delete a painting', async () => {
       expect(res.body.message).toEqual('painting deleted');
     });
 }, 15000);
-/* test('post a movie with res', async () => {
-  const newPainting = {
-    name: 'Esto es un test',
-    date: '2022',
-    authorId: '637f888f08f271a0f963c646',
-  };
-  const res = await request(app).post('/api/paintings').send(newPainting);
-  await expect(res.statusCode).toEqual(201);
-  await expect(res.body.message).toEqual('Created Painting');
+
+/* afterAll(async () => {
+  await app.close();
+  await mongoose.close();
+});
+afterAll(async () => {
+  await new Promise((resolve) =>
+    setTimeout(() => resolve(mongoose.close()), 200)
+  );
 }); */
